f04514dcd568f3abab9c00a4138e55bfdb2aa6e5
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-02-18 21:58:03 +0200

    Початок розробки головного персонажа. Прибрав зайве

---
+++ Source/Project_GZ/Base_Starting_Location_Game_Mode.cpp
@@ -0,0 +1,1 @@
+#include "Base_Starting_Location_Game_Mode.h"
---
+++ Source/Project_GZ/Base_Starting_Location_Game_Mode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Base_Starting_Location_Game_Mode.generated.h"
+
+//------------------------------------------------------------------------------------------------------------
+UCLASS()
+class PROJECT_GZ_API ABase_Starting_Location_Game_Mode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
+//------------------------------------------------------------------------------------------------------------
