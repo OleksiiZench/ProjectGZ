52c11d7d2e7c004c41485389f39cff3d2f891d05
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-02-26 11:11:18 +0200

    Логіка з Event Graph з ABP_Main_Chararcer перенесена на C++ рівень

--- Source/Project_GZ/Private/Base_Anim_Main_Character.cpp
+++ Source/Project_GZ/Private/Base_Anim_Main_Character.cpp
@@ -1,1 +1,24 @@
 #include "Base_Anim_Main_Character.h"
+#include "Base_Main_Character.h"
+
+//------------------------------------------------------------------------------------------------------------
+void UBase_Anim_Main_Character::NativeUpdateAnimation(float Delta_Seconds)
+{
+	Super::NativeUpdateAnimation(Delta_Seconds);
+
+	if (!Main_Character)
+	{
+		Main_Character = Cast<ABase_Main_Character>(TryGetPawnOwner());
+		
+		if (!Main_Character)
+			return;
+	}
+
+	Velocity = Main_Character->GetVelocity().Size();
+
+	if ((Main_Character->GetCapsuleComponent()->GetScaledCapsuleHalfHeight() ) < 80.0f)
+		Is_Crawling = true;
+	else
+		Is_Crawling = false;
+}
+//------------------------------------------------------------------------------------------------------------
--- Source/Project_GZ/Public/Base_Anim_Main_Character.h
+++ Source/Project_GZ/Public/Base_Anim_Main_Character.h
@@ -2,7 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+
 #include "Base_Anim_Main_Character.generated.h"
+
+class ABase_Main_Character;
 
 //------------------------------------------------------------------------------------------------------------
 UCLASS()
@@ -11,10 +14,15 @@ class PROJECT_GZ_API UBase_Anim_Main_Character : public UAnimInstance
 	GENERATED_BODY()
 
 public:
+	virtual void NativeUpdateAnimation(float Delta_Seconds) override;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations") bool Is_Crawling;
 
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations") float Velocity;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animations") bool Is_Falling;
+
+private:
+	ABase_Main_Character *Main_Character;
 };
 //------------------------------------------------------------------------------------------------------------
