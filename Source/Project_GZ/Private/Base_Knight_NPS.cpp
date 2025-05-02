8b67a63d52cf7f98e2f77c1ad6b4a98ee2698067
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-05-02 20:13:32 +0300

    Додав: NPS Knight реагує на натискання 'F', Реалізацію надпису над головою 'F'

--- Source/Project_GZ/Private/Base_Knight_NPS.cpp
+++ Source/Project_GZ/Private/Base_Knight_NPS.cpp
@@ -1,9 +1,41 @@
-#include "Base_Knight_NPS.h"
+﻿#include "Base_Knight_NPS.h"
 
 //------------------------------------------------------------------------------------------------------------
 ABase_Knight_NPS::ABase_Knight_NPS()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	// 1. Додаємо/Налаштовуємо компонент сфери
+	Sphere_Component = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	Sphere_Component->SetupAttachment(RootComponent);
+	Sphere_Component->SetWorldScale3D(FVector(8.0f));
+
+	// 2. Приєднуємо компонент віджета для відрображення 'F' до сокета голови
+	Can_Interact_Widget_Component = CreateDefaultSubobject<UWidgetComponent>(TEXT("Can_Interact_Widget") );
+
+	if (GetMesh() )
+		Can_Interact_Widget_Component->SetupAttachment(GetMesh(), TEXT("Head_Socket"));
+
+	if (Can_Interact_Widget_Class)
+		Can_Interact_Widget_Component->SetWidgetClass(Can_Interact_Widget_Class);
+
+	Can_Interact_Widget_Component->SetDrawSize(FVector2D(200.0f, 50.0f));
+}
+//------------------------------------------------------------------------------------------------------------
+void ABase_Knight_NPS::Tick(float Delta_Time)
+{
+	Super::Tick(Delta_Time);
+
+	// 1. 
+	Main_Character = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0);
+
+	if (Main_Character)
+		Is_Overlaping = IsOverlappingActor(Main_Character);
+
+	if (Is_Overlaping)
+		Can_Interact_Widget_Component->SetVisibility(true);
+	else
+		Can_Interact_Widget_Component->SetVisibility(false);
 }
 //------------------------------------------------------------------------------------------------------------
 void ABase_Knight_NPS::Interact()
--- Source/Project_GZ/Public/Base_Knight_NPS.h
+++ Source/Project_GZ/Public/Base_Knight_NPS.h
@@ -2,6 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Components/WidgetComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
 
 #include "IInteractable.h"
 
@@ -16,6 +20,16 @@ class PROJECT_GZ_API ABase_Knight_NPS : public ACharacter, public IInteractable
 public:
 	ABase_Knight_NPS();
 
+	virtual void Tick(float Delta_Time) override;
+
 	virtual void Interact() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Widgets") UWidgetComponent* Can_Interact_Widget_Component;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widgets") TSubclassOf<UUserWidget> Can_Interact_Widget_Class;
+	UPROPERTY(VisibleAnywhere, Category = "Components") USphereComponent *Sphere_Component;
+
+private:
+	bool Is_Overlaping;
+	ACharacter *Main_Character;
 };
 //------------------------------------------------------------------------------------------------------------
