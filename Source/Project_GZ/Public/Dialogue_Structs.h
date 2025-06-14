06898ad574325b195477d0edeb919dc4f0cb166a
Oleksii Zenchenko <olekzenchenko@gmail.com>
2025-06-14 12:28:04 +0300

    Початок створення вікна діалогу

---
+++ Source/Project_GZ/Public/Dialogue_Structs.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+
+#include "Dialogue_Structs.generated.h"
+
+// FDialogue_Option
+//------------------------------------------------------------------------------------------------------------
+USTRUCT(BlueprintType)
+struct FDialogue_Option
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int32 Nex_Node_ID;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FText Option_Text;
+};
+//------------------------------------------------------------------------------------------------------------
+
+
+
+
+// FDialogue_Node
+//------------------------------------------------------------------------------------------------------------
+USTRUCT(BlueprintType)
+struct FDialogue_Node : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) bool bIs_Conditional;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) int32 Node_ID;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FText Speaker_Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) FText Dialogue_Text;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) TArray<FDialogue_Option> Options;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (EditConditoin = "bIs_Conditional") ) FName Condition_Tag;
+};
+//------------------------------------------------------------------------------------------------------------
